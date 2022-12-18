8b198931637cc25dfafbf72e4f875485d5f3f849
sheep-lambs <z2104025166@163.com>
2022-12-18 23:02:37 +0800

    随写

--- J_2022-12-18/src/Test.java
+++ J_2022-12-18/src/Test.java
@@ -6,34 +6,64 @@
  * Time: 14:23
  */
 class ListNode {
-    int val;
-    ListNode next;
+    class Node {
+        int val;
+        Node next;
 
-    public ListNode(int val) {
-        this.val = val;
+        public Node(int val) {
+            this.val = val;
+        }
     }
-    void a () {
-        ListNode node = new ListNode(1);
+    public Node head;
+    public Node getHead() {
+        return this.head;
     }
+
+    {
+        Node node = new Node(1);
+        head = node;
+    }
+
     void add(int val) {
-        ListNode node = new ListNode(val);
-
+        Node node1 = new Node(val);
+        node1.next = head;
+        head = node1;
     }
-}
-public class Test {
-    public ListNode reverseList(ListNode head) {
-        ListNode cur = head;
+
+    void disPlay() {
+        Node cur = head;
+        while(cur != null) {
+            System.out.print(cur.val);
+            cur = cur.next;
+        }
+    }
+     Node reverseList(Node head) {
+        Node cur = head;
         head = head.next;
-        ListNode p = head.next;
+        Node p = head.next;
+        cur.next = null;
         do{
             head.next = cur;
             cur = head;
             head = p;
             p = p.next;
         }while(p != null);
-        return cur;
+        head.next = cur;
+        return head;
     }
-    public static void main(String[] args) {
 
+
+}
+public class Test {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode();
+        listNode.add(2);
+        listNode.add(3);
+        listNode.add(4);
+        listNode.add(5);
+        listNode.add(6);
+        listNode.reverseList(listNode.getHead());
+        listNode.disPlay();
     }
 }
