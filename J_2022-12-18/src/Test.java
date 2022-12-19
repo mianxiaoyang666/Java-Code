44b15e965691db5d7d8953ccc4b80c82cad8702c
sheep-lambs <z2104025166@163.com>
2022-12-19 23:53:57 +0800

    随写代码

--- J_2022-12-18/src/Test.java
+++ J_2022-12-18/src/Test.java
@@ -2,68 +2,29 @@
  * Created with IntelliJ IDEA.
  * Description:
  * User: 21040
- * Date: 2022-12-18
- * Time: 14:23
+ * Date: 2022-12-19
+ * Time: 15:50
  */
-class ListNode {
-    class Node {
-        int val;
-        Node next;
+class A {
+    public void a() {
 
-        public Node(int val) {
-            this.val = val;
-        }
     }
-    public Node head;
-    public Node getHead() {
-        return this.head;
-    }
+}
 
-    {
-        Node node = new Node(1);
-        head = node;
+class B extends A{
+    public void b() {
+        int a = 0;
+        a = 1;
     }
+}
 
-    void add(int val) {
-        Node node1 = new Node(val);
-        node1.next = head;
-        head = node1;
-    }
-
-    void disPlay() {
-        Node cur = head;
-        while(cur != null) {
-            System.out.print(cur.val);
-            cur = cur.next;
-        }
-    }
-     Node reverseList(Node head) {
-        Node cur = head;
-        head = head.next;
-        Node p = head.next;
-        cur.next = null;
-        do{
-            head.next = cur;
-            cur = head;
-            head = p;
-            p = p.next;
-        }while(p != null);
-        head.next = cur;
-        return head;
-    }
-
+class C extends B {
 
 }
 public class Test {
-
     public static void main(String[] args) {
-        ListNode listNode = new ListNode();
-        listNode.add(2);
-        listNode.add(3);
-        listNode.add(4);
-        listNode.add(5);
-        listNode.add(6);
-        listNode.reverseList(listNode.getHead());
-        listNode.disPlay();
+        A a = new C();
+        a.a();
+
     }
 }
