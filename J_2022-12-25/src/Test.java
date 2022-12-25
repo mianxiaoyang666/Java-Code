9745f61ed5c45354de7afa543497c9844f8f735e
mianxiaoyang666 <2104025166@qq.com>
2022-12-25 23:49:14 +0800

    随写代码

---
+++ J_2022-12-25/src/Test.java
@@ -0,0 +1,16 @@
+import test.Test1;
+
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 21040
+ * Date: 2022-12-25
+ * Time: 17:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        Test1 test = new Test1();
+    }
+}
---
+++ J_2022-12-25/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 21040
+ * Date: 2022-12-25
+ * Time: 17:49
+ */
+public class Test {
+}
+public class Test1 {
+
+}
