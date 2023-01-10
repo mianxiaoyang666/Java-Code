3df64e6d86c67b4d4fb4704effeaf2ad72972c81
mianxiaoyang666 <2104025166@qq.com>
2023-01-10 23:34:53 +0800

    随写

--- J_2023-1-9/src/MyStack.java
+++ J_2023-1-9/src/MyStack.java
@@ -1,3 +1,4 @@
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -9,50 +10,53 @@ import java.util.Queue;
  * Time: 22:28
  */
 class MyStack {
-    Queue<Integer> pushList;
-    Queue<Integer> popList;
+    Queue<Integer> list1;
+    Queue<Integer> list2;
     public MyStack() {
-        pushList = new LinkedList<Integer> ();
-        popList = new LinkedList<Integer>();
+        list1 = new LinkedList<Integer> ();
+        list2 = new LinkedList<Integer> ();
     }
 
     public void push(int x) {
-        pushList.offer(x);
+        list2.offer(x);
+        while(!list1.isEmpty()) {
+            list2.offer(list1.poll());
+        }
+        Queue<Integer> tmp = list2;
+        list2 = list1;
+        list1 = tmp;
     }
 
     public int pop() {
-        if(popList.isEmpty()) {
-            while(!pushList.isEmpty()) {
-                popList.offer(pushList.poll());
-            }
-        }
-        return popList.poll();
+        return list1.poll();
     }
 
     public int top() {
-        if(popList.isEmpty()) {
-            while(!pushList.isEmpty()) {
-                popList.offer(pushList.poll());
-            }
-        }
-        return popList.peek();
+        return list1.peek();
     }
 
     public boolean empty() {
-        if(pushList.isEmpty() && popList.isEmpty()) {
-            return true;
-        }
-        return false;
+        return list1.isEmpty();
     }
 
     public static void main(String[] args) {
         MyStack myStack = new MyStack();
         myStack.push(1);
         myStack.push(2);
-        int a = myStack.top();
-        System.out.println(a);
+        myStack.push(3);
+        int a = myStack.pop();
+        a = myStack.pop();
         a = myStack.pop();
         System.out.println(a);
         System.out.println(myStack.empty());
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(2);
+        deque.offer(1);
+        deque.addLast(3);
+        deque.pollFirst();
+        deque.pollLast();
+        System.out.println(deque);
+        int[] arr;
+        arr = new int[2];
     }
 }
