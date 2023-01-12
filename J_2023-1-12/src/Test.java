3a94aa9dfdf3821ecc29d68e43cc78190f1980de
mianxiaoyang666 <2104025166@qq.com>
2023-01-12 23:33:27 +0800

    随写

---
+++ J_2023-1-12/src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 21040
+ * Date: 2023-01-12
+ * Time: 9:54
+ */
+public class Test {
+    public void pop() {
+
+    }
+    public boolean isEmpty() {
+        return usedSize == 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
