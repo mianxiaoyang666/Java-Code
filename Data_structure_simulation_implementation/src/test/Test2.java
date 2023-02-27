06eee8f524f09d4b8fd2e5f0f458b8b0e083d0b0
mianxiaoyang666 <2104025166@qq.com>
2023-02-27 11:25:00 +0800

    测试

--- Data_structure_simulation_implementation/src/test/Test2.java
+++ Data_structure_simulation_implementation/src/test/Test2.java
@@ -11,11 +11,44 @@ import binarytree.CharBinaryTree;
  */
 public class Test2 {
     public static void main(String[] args) {
+        char[] ch = new char[]{'h','e','l','l','o'};
+        String a = new String(ch);
+        a.intern();
+        String b = "hello";
+        System.out.println(a == b);
+    }
+    public static void main3(String[] args) {
+        String a = "hello";
+        String b = "hello" + "World";
+        String c = "helloWorld";
+        String d = new String("haha");
+    }
+//    public static void main2(String[] args) {
+//        String a = "hello";
+//        String b = "hello";
+//        System.out.println(a == b);
+//
+//        String aa = new String("haha");
+//        String bb = new String("haha");
+//        System.out.println(aa == bb);
+//    }
+
+    public static void main2(String[] args) {
+        String a = new String("aaa");
+        String b = "aaa";
+        System.out.println(a == b);
+        String c = "bbb";
+        String d = new String("ccc");
+        String e = "ccc";
+    }
+    public static void main1(String[] args) {
         CharBinaryTree charBinaryTree = new CharBinaryTree();
         charBinaryTree.root= charBinaryTree.createTree();
         charBinaryTree.root2 = charBinaryTree.createTree2();
         boolean a = charBinaryTree.isSubtree(charBinaryTree.root, charBinaryTree.root2);
         System.out.println(a);
+        String s = "aaa";
+        s.intern();
     }
 
 }
